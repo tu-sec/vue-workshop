d8c09e01e1986a135f60ea2e78ed39f59586811f
Emily Vogel <eav990@gmail.com>
2021-08-22 19:01:38 -0400

    Write example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <p>Welcome to your first Vue App!</p>
+    <button @click="toggleVisible()">Toggle Visibility</button>
+    <p v-if="visible">Welcome to your first Vue App!</p>
   </div>
 </template>
 
@@ -8,14 +9,29 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  visible = false; // Variable referenced in the v-if on line 4
+
+  /**
+   * Toggles the visibility on and off of the welcome message
+   * 
+   * Used by the click handler on line 3
+   */
+  toggleVisible(): void {
+    this.visible = !this.visible;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+button {
+  display: block;
+  margin-bottom: 20px;
+}
 .home {
   height: 100vh;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
