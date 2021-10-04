959617ccc82d6da7f56a1a6c630e3a6f166ad04c
Emily Vogel <eav990@gmail.com>
2021-10-04 19:12:48 -0400

    Working component and loop code

---
+++ src/components/NumberComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This component's number is {{ prop }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class NumberComponent extends Vue {
+  @Prop({ required: false })
+  prop: number | undefined;
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="home">
-    <p>Welcome to your first Vue App!</p>
+    <p>This is a for loop without a component:</p>
+    <ul>
+      <li v-for="a in arr" :key="a">{{ a }}</li>
+    </ul>
+
+    <p>This is a for loop with a component:</p>
+    <NumberComponent v-for="(a, index) in arr" :key="index" :prop="a"></NumberComponent>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import NumberComponent from '@/components/NumberComponent.vue';
 
-@Component
-export default class Home extends Vue {}
+@Component({
+  components: { NumberComponent }
+})
+export default class Home extends Vue {
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // The array we are referencing for the v-for on line 5 and 9
+}
 </script>
 
 <style lang="scss" scoped>
